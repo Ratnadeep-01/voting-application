792a9625db9a8cd15dbf42bf289a436f1801d799
Ratnadeep-01 <ratnadeep4881@gmail.com>
2026-02-15 14:42:24 +0530

    Remove trailing slash from CORS origin URL

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ const PORT = process.env.PORT || 5000;
 
 app.use(
   cors({
-    origin: "https://votex-lyart.vercel.app/",
+    origin: "https://votex-lyart.vercel.app",
     credentials: true
   })
 );
