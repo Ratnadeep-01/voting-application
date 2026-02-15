7922db7040096fd5782f7dfce9c78f342a87a088
Ratnadeep-01 <ratnadeep4881@gmail.com>
2026-02-15 13:25:09 +0530

    Update API base URL to production endpoint

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:5000',
+    baseURL: 'https://voting-application-7io9.onrender.com',
 });
 
 // Request interceptor to add the JWT token to every request
